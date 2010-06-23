2eaa1409002408bfa438b8aa07135e0c14a5bb06
Joseph Larsen <jlarsen@Delorum8-4.local>
2010-06-23 14:21:31 -0600

    NEW: client and server are talking

--- bin/client.rb
+++ bin/client.rb
@@ -1,11 +1,12 @@
 require File.dirname(__FILE__) + '/../lib/metrics.rb'
 require File.dirname(__FILE__) + '/../lib/daemon.rb'
 require File.dirname(__FILE__) + '/../lib/parseconfig.rb'
+require 'socket'
 
 class WatchTowerClient < Daemon::Base
   WorkingDirectory = File.expand_path(File.dirname(__FILE__))  
-  
-  #static methods
+  @log = STDOUT
+  #static methods - this is really just a singleton though, so there are no instance methods
   class << self 
     def start
       conf = ParseConfig.new(WorkingDirectory + '/../etc/client.conf')
@@ -16,27 +17,39 @@ class WatchTowerClient < Daemon::Base
       interval = 5 if interval == 0
       server_ip = conf.params['server_ip']
       server_port = conf.params['server_port']
-    
+      
+      self.connect_to_server(server_ip, server_port)
       #Log client start
-      @f = File.open('stats.log', 'a') 
-      @f.puts "===START=== #{server_ip}: #{server_port}"
-      @f.flush
-    
       loop do
         sleep interval
-        @f.puts "CPU: #{metric.get_cpu_usage}, Mem: #{metric.get_mem_usage}, Load: #{metric.get_load_average}"
-        @f.flush
+        self.send_message_to_server(metric.get_cpu_usage, metric.get_mem_usage, metric.get_load_average)
       end
     end
   
     def stop
-      @f.puts "===STOP==="
-      @f.close
+      self.disconnect_from_server
+    end
+    
+    def connect_to_server(server_ip, server_port)
+      @socket = TCPSocket.open(server_ip,server_port)
+      @socket.puts "info|1|123"
+      @log.puts "===START=== #{server_ip}: #{server_port}"
+      @log.flush
+    end
+    
+    def send_message_to_server(cpu, mem, load)
+      @socket.puts "data|#{cpu}|#{mem}|#{load}"
+      @log.puts "to server - CPU = #{cpu}, Memory = #{mem}, Load = #{load}"
+      @log.flush
+    end
+    
+    def disconnect_from_server
+      @socket.puts "stop"
+      @socket.close
+      @log.puts "===STOP==="
+      @log.close
     end
   end
-  
-  #instance methods go here
-  
 end
 
 WatchTowerClient.daemonize
--- bin/server.rb
+++ bin/server.rb
@@ -1,18 +1,71 @@
 require File.dirname(__FILE__) + '/../lib/daemon.rb'
 require File.dirname(__FILE__) + '/../lib/parseconfig.rb'
+require 'socket'
 
 class WatchTowerServer < Daemon::Base
   WorkingDirectory = File.expand_path(File.dirname(__FILE__))  
   #static methods
   class << self
     def start
+      conf = ParseConfig.new(WorkingDirectory + '/../etc/server.conf')
+      port = conf.params['server_port']
+      
+      @listener = TCPServer.open(port)
+      @sockets = [@listener]
+      log = STDOUT
+      while true
+        # listen for connections
+        ready = select(@sockets)
+        readable = ready[0]
+        
+        readable.each do |socket|
+          # on connection - create thread
+          if socket == @listener
+            client = @listener.accept
+            @sockets << client
+            client.puts "Welcome"
+            log.puts "Accepted connection from #{client.peeraddr[2]}"
+          else
+            #   per thread - listen for data
+            input = socket.gets
+            
+            if !input
+              log.puts "Client on #{socket.peeraddr[2]} disconnected."
+              @sockets.delete(socket)
+              socket.close
+              next
+            end
+            
+            input.chop!
+            #     on data - split on |
+            if (input == 'stop')
+              socket.puts("bye")
+              log.puts "Closing connection to #{socket.peeraddr[2]}"
+              @sockets.delete(socket)
+              socket.close
+            else
+              log.puts "I got #{input} from #{socket.peeraddr[2]}"
+            end
+            #     if data is first, then output "to database - blah"
+            #     if info is first, then output "info from the client - blah"
+            #     if stop is first, then output "goodbye dear PID", kill thread and close socket.
+            
+          end
+        end
+      end
     end
     
     def stop
+      #tell all the connections to die
+      @sockets.each do |socket|
+        socket.puts("shutdown")
+        socket.close
+      end
+      @listener.close
     end
   end
 
   #instance methods go here
 end
 
-WatchTowerClient.daemonize
+WatchTowerServer.daemonize
