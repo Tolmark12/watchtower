1e3c5b334ec7a70bd1c3cf8ce2441e137d50dab3
Joseph Larsen <jlarsen@Delorum8-6.local>
2010-06-30 14:27:10 -0600

    NEW: refactoring

--- bin/server.rb
+++ bin/server.rb
@@ -10,215 +10,215 @@ require 'thread'
 # Boilerplate to allow custom ruby functionality if they are able
 require File.dirname(__FILE__) + '/init.rb' if File.exists? File.dirname(__FILE__) + '/init.rb'
 
-class WatchTowerServer < Daemon::Base
-  WorkingDirectory = File.expand_path(File.dirname(__FILE__))  
-  @data = Array.new
-  @data_lock = Mutex.new
-  @listener_lock = Mutex.new
-  @rules_lock = Mutex.new
-  @averages = Array.new
-  @cluster_map = Hash.new
-  #static methods
-  class << self
-    attr_accessor :data, :averages
+module WatchTower
+  # Cluster, Server, and Rules
+  class Cluster
+
+    def initialize(name)
+      @data_lock = Mutex.new
+      @rules_lock = Mutex.new
+      @clients = []
+      @latest = Hash.new
+      @name = name
+      @rules, @metrics = Rule.get_rules_for_cluster(name)
+      @data = Array.new
+      @data.max_size = Server.max_size
+      @averages = Array.new
+      @averages.max_size = Server.max_size
+      puts "Cluster #{name} is initialized"
+    end
     
-    def server_interval
+    def add_client(socket, group_name)
+      #create a thread that listens and responds to the client
+      @clients << socket
+      metric_lines = get_metrics_for_client(group_name)
+      puts "There are #{@metrics.size} metrics"
+      if metric_lines.size > 0
+        socket.puts "info-$-#{Server.interval}-$-#{metric_lines.join("-$$-")}"
+        puts "Sending #{metric_lines.join('\/')}"
+      else
+        socket.puts "error-$-There are no rules setup yet for your cluster"
+        puts "Error, no metrics found for #{@name}, #{group_name}"
+      end
+
+      Thread.new do
+        while true
+          input = socket.gets
+
+          if !input
+            break
+          end
+
+          input.chop!
+          input = input.split("|")
+          type = input[0]
+          input.shift
+
+          if type != "data"
+            puts "Invalid data from client: #{type} != data"
+          else
+            @data_lock.lock
+            @latest[socket.peeraddr[2]] = Hash.new
+            i = 0
+            @metrics.each do |metric|
+              @latest[socket.peeraddr[2]][metric] = input[i]
+              i = i + 1
+            end
+            @data_lock.unlock
+
+            puts "#{@name}-#{socket.peeraddr[2]} -- #{input.join('|')}"
+          end
+        end
+        puts "Client on #{socket.peeraddr[2]} disconnected from cluster #{@name}."
+        @clients.delete(socket)
+        socket.close
+      end
+    end
+
+    def get_metrics_for_client(group)
+      metric_lines = []
+      @metrics.each do |metric|
+        if group != ""
+          metric_lines << Server.metrics.params[group][metric]
+        else
+          metric_lines << Server.metrics.params[metric]
+        end
+      end
+      return metric_lines
+    end
+    
+    def on_server_interval
       @data_lock.lock
       @last = @latest
       @latest = Hash.new
-      
-      @averages.push_safe(average_data(@last))
-      if @averages[0].size == 0
-        @averages.pop
-        @data_lock.unlock
-        return
+      average = average_data(@last)
+      if average.size > 0
+        @averages.push_safe(average)
       end
-      @data.push_safe(@latest) #if @last.size > 0
+#      @data.push_safe(@latest) #if @last.size > 0
       @data_lock.unlock
-      #for each cluster
-      @averages[0].keys.each do |cluster|
-        #check to see if u decrement check in it reaches 0 (returns 1 for true)
-        @rules.each do |rule|
-          if (rule.decrement_check_in == true)
-            @rules_lock
-              #check the rule, which will fire it on Actions if it's met
-              rule.check_rule(@averages, cluster)
-            @rules_unlock
-          end
+
+      #check to see if u decrement check in it reaches 0 (returns 1 for true)
+      @rules.each do |rule|
+        if (rule.decrement_check_in == true)
+          @rules_lock.lock
+            #check the rule, which will fire it on Actions if it's met
+            rule.check_rule(@averages)
+          @rules_lock.unlock
         end
       end
     end
     
     def average_data(last)
-      clusters = Hash.new
+      average = Hash.new
       string = "average "
       size = 0
-      last.each do |clustername, cluster|
-        metric_names = get_clusters_metrics(clustername)
-        puts "Metric names for #{clustername} = #{metric_names.join(',')}"
-        average_metrics = Hash.new
-        metric_names.each do |metric_name|
-          total_metric_value = 0
-          cluster.each do |hostname, client|
-            total_metric_value += client[metric_name].to_f
-          end
-          average_metrics[metric_name] = total_metric_value / cluster.size
-          string = string + "#{metric_name}: #{average_metrics[metric_name]}, "
-          size = cluster.size
-          @log.flush
+      @metrics.each do |metric_name|
+        total_metric_value = 0
+        last.each do |hostname, client|
+          total_metric_value += client[metric_name].to_f
         end
-        clusters[clustername] = average_metrics
+        average[metric_name] = total_metric_value / last.size
+        string = string + "#{metric_name}: #{average[metric_name]}, "
+        size = last.size
+        puts "#{@name}: #{string} with #{size} clients"
       end
-      puts "#{string} with #{size} clients"
-      
-      return clusters
-    end
-
-    def get_clusters_metrics(cluster)
-      if Rule.clusters_rules[cluster] == nil
-        cluster_rules = []
-      else
-        cluster_rules = Rule.clusters_rules[cluster]
-      end
-      if Rule.clusters_rules['all'] == nil
-        clusters_rules_all = []
-      else
-        clusters_rules_all = Rule.clusters_rules['all']
-      end
-      metrics = cluster_rules + clusters_rules_all      
-    end
-
-    def get_metrics_for_client(client_type, metrics)
-      metric_lines = Array.new
-      metrics.each do |metric_name|
-        puts "Looking up #{client_type} sub #{metric_name}"
-        puts "Found #{@metrics.params[client_type][metric_name]}"
-        metric_lines << @metrics.params[client_type][metric_name]
-      end      
-      metric_lines
-    end
-    
-    def handle_client(socket)
-      while true
-        input = socket.gets
-
-        if !input
-          break
-        end
-
-        input.chop!
-        #     on data - split on |
-        input = input.split("|")
-        type = input[0]
-        input.shift
-        case type
-        when "data"
-          @data_lock.lock
-          @latest[@cluster_map[socket.peeraddr[2]]] ||= Hash.new
-          @latest[@cluster_map[socket.peeraddr[2]]][socket.peeraddr[2]] = Hash.new
-          i = 0
-          @client_metrics[socket.peeraddr[2]].each do |metric|
-            @latest[@cluster_map[socket.peeraddr[2]]][socket.peeraddr[2]][metric] = input[i]
-            puts "Got metric #{metric} = #{input[i]}"
-            i = i + 1
-          end
-#          @latest[@cluster_map[socket.peeraddr[2]]][socket.peeraddr[2]]['mem'] = input[1]
-#          @latest[@cluster_map[socket.peeraddr[2]]][socket.peeraddr[2]]['load'] = input[2]
-          @data_lock.unlock
-          @log.puts "I got #{input.join('|')} from #{@cluster_map[socket.peeraddr[2]]}-#{socket.peeraddr[2]} #{@data.size}"
-        when "info"
-          @log.puts "I got #{input[0]} from #{socket.peeraddr[2]}"
-          @log.flush
-          if (input[0] == nil || input[0] == "")
-            input[0] = "default"
-          end
-          if (input[1] == nil || input[1] == "")
-            input[1] = "default"
-          end
-          
-          metrics = get_clusters_metrics(input[0])
-          @client_metrics ||= Hash.new
-          @client_metrics[socket.peeraddr[2]] = metrics
-          metric_lines = get_metrics_for_client(input[1], metrics)
-          if metrics.size > 0
-            socket.puts "info-$-#{@interval}-$-#{metric_lines.join("-$$-")}"
-            @log.puts "Sending #{metric_lines.join('\/')}"
-          else
-            socket.puts "error-$-There are no rules setup yet for your cluster"
-            @log.puts "Error"
-          end
-          @cluster_map[socket.peeraddr[2]] = input[0]
-          @log.puts "I got info from #{socket.peeraddr[2]}"
-        end
-      end
-      
-      #after the client disconnects
-      @log.puts "Client on #{socket.peeraddr[2]} disconnected."
-      @listener_lock.lock
-        @sockets.delete(socket)
-      @listener_lock.unlock
-      socket.close
-    end
-    
-    def start
-      @averages.max_size = 100
-      @data.max_size = 100
-      
-      conf = ParseConfig.new(WorkingDirectory + '/../etc/server.conf')
-      @metrics = ParseConfig.new(WorkingDirectory + '/../etc/metrics.conf')
-      @rules = Rule.read_in_rules(WorkingDirectory + '/../etc/rules.conf')
-      port = conf.params['server_port']
-      @interval = conf.params['interval'].to_i
-      @interval = 5 if @interval == 0
-
-      max_size = conf.params['max_size'].to_i
-      if (max_size == 0)
-        max_size = 100
-      end
-      
-      @listener = TCPServer.open(port)
-      @sockets = []
-      @log = STDOUT
-      log = STDOUT
-      
-      #spawn a thread to run on an interval
-      @latest = Hash.new
-      @server_interval_thread = Thread.new {
-        sleep @interval
-        while true
-          start = Time.now
-          if @sockets.size > 0
-            server_interval()
-          end
-          newinterval = @interval - (Time.now - start)
-          while newinterval <= 0
-            @log.puts "Missed a beat by #{newinterval} seconds"
-            newinterval += @interval
-          end
-          sleep (newinterval)
-        end
-      }
-      
-      # listen for connections
-      while true
-        client = @listener.accept
-        Thread.start(client) do |c|
-          @listener_lock.lock
-          @sockets << c
-          @listener_lock.unlock
-          log.puts "Accepted connection from #{client.peeraddr[2]} I sent them interval|#{@interval}"
-          handle_client(c)
-        end
-      end        
-    end
-    
-    def stop
-      @listener.close #doing this will automatically close all the sockets
+      return average
     end
   end
+  
+  class Server < Daemon::Base
+    WorkingDirectory = File.expand_path(File.dirname(__FILE__))  
+    @@clusters = Hash.new
+    #static methods
+    class << self
+      attr_reader :metrics, :interval, :max_size
+      def initialize
+        @listener_lock = Mutex.new
+      end
 
-  #instance methods go here
+      def start_server_interval
+        @server_interval_thread = Thread.new {
+          sleep @interval
+          while true
+              start = Time.now
+            @@clusters.each do |clustername, cluster|
+              cluster.on_server_interval
+            end
+            new_interval = @interval - (Time.now - start)
+            while new_interval <= 0
+              puts "Missed a beat by #{new_interval} seconds"
+              new_interval += @interval
+            end
+            sleep(new_interval)
+          end
+        }
+      end
+
+      def listen_for_connections
+        while true
+          client = @listener.accept
+          Thread.start(client) do |c|
+            input = c.gets
+
+            if !input
+              break
+            end
+
+            input.chop!
+            input = input.split("|")
+            type = input[0]
+            input.shift
+
+            if type != "info"
+              puts "ERROR- the client isn't speaking my language - he said #{type} rather than giving me info"
+            else
+              input[0] ||= "default"
+              input[0] = "default" if input[0] == ""
+              if @@clusters.include?(input[0]) == false
+                puts "Creating cluster #{input[0]}"
+                @@clusters[input[0]] = Cluster.new(input[0])
+              else
+                puts "Clusters already has #{input[0]}"
+              end
+
+              @@clusters[input[0]].add_client(c, input[1])
+              puts "Accepted connection from #{c.peeraddr[2]} I sent them to cluster #{input[0]}"
+            end
+          end
+        end
+      end
+
+      def start
+        #set the file for Rule to use
+        Rule.setup_rules(WorkingDirectory + '/../etc/rules.conf')
+
+        # Read in the config file and setup config variables
+        conf = ParseConfig.new(WorkingDirectory + '/../etc/server.conf')
+        @metrics = ParseConfig.new(WorkingDirectory + '/../etc/metrics.conf')
+        port = conf.params['server_port']
+        @interval = conf.params['interval'].to_i
+        @interval = 5 if @interval == 0
+
+        @max_size = conf.params['max_size'].to_i
+        if (@max_size == 0)
+          @max_size = 100
+        end
+      
+        @listener = TCPServer.open(port)
+
+        #spawn a thread to run on an interval
+        @latest = Hash.new
+
+        start_server_interval #starts up a thread that loops forever
+        listen_for_connections #this runs forever (making this a daemon)
+      end
+    
+      def stop
+        @listener.close #doing this will automatically close all the sockets
+      end
+    end
+  end
 end
-
 Thread.abort_on_exception = true
-WatchTowerServer.daemonize
+WatchTower::Server.daemonize
--- lib/rules.rb
+++ lib/rules.rb
@@ -1,131 +1,134 @@
-class Rule
-  @@clusters_rules = Hash.new
-  class << self 
-    def clusters_rules
-      @@clusters_rules
-    end
-    
-    def read_in_rules(file_to_read)
-      rules = []
-      open(file_to_read).each do |line| 
-        line.strip!
-        if line.length> 0
-          unless (/^\#/.match(line))
-            rules << Rule.new(line)
-          end
-        end
+require 'thread'
+module WatchTower
+  class Rule
+    @@file_lock = Mutex.new
+
+    class << self
+
+      def setup_rules(file_to_read)
+        @@file_name = file_to_read
       end
-      return rules
-    end
-  end
-  
-  def initialize(rule)
-    @error = STDOUT
-    regex = /^([a-z_]+) if ([a-z]+) (.*)is ([>=<]+) ([0-9]+) for ([0-9]+) intervals then (call|run|log) (.*) and wait ([0-9]+) intervals/
-    if rule =~ regex
-      rule.scan(regex) {|name, metric, clusters, operator, value, threshold, action_type, action, wait_interval|
-        @rule_name = name
-        @metric = metric
-        @operator = operator
-        @value = value.to_i
-        clusters.slice!("of ")
-        clusters.strip!
-        @clusters = clusters.split(" or ")
-        if @clusters.size == 0
-          if !@@clusters_rules.include?("all")
-            @@clusters_rules["all"] = Array.new
-          end
-          if !@@clusters_rules["all"].include? @metric
-            @@clusters_rules["all"] << metric
-          end
-        else
-          @clusters.each do |cluster|
-            if !@@clusters_rules.include?(cluster)
-              @@clusters_rules[cluster] = Array.new
-            end
-            if !@@clusters_rules[cluster].include? @metric
-              @@clusters_rules[cluster] << @metric
+
+      def get_rules_for_cluster(cluster_name)
+        rules = []
+        metric_names = []
+
+        @@file_lock.lock
+        open(@@file_name).each do |line|
+          line.strip!
+          if line.length> 0
+            unless (/^\#/.match(line))
+              #verify that this rule works for this cluster
+              regex =
+              line.scan(/^([a-z_]+ if [a-z]+ )(.*?)is(.*)/) {|worthless, clusters, worthless|
+                clusters.slice!("of ")
+                clusters.strip!
+                cluster_array = clusters.split(" or ")
+                
+                if cluster_array.size == 0 || cluster_array.include?(cluster_name) || cluster_array.include?("all")
+                  #if it does, make it into a rule and return it.
+                  rule = Rule.new(line, cluster_name) #assign it to a variable so we can also get the metric name
+                  rules << rule
+                  puts "#{rule.rule_name},#{cluster_name}"
+                  # add the metric name if it's not already there
+                  if metric_names.include?(rule.metric) == false
+                    metric_names << rule.metric
+                  end
+                end
+              }
             end
           end
         end
-        @threshold = threshold.to_i
-        @action_type = action_type
-        @action = action
-        @wait_interval = wait_interval.to_i 
+        @@file_lock.unlock
+        return rules, metric_names
+      end
+    end
+
+    attr_reader :metric, :rule_name
+
+    def initialize(rule, cluster_name)
+      @error = STDOUT
+      regex = /^([a-z_]+) if ([a-z]+) .*is ([>=<]+) ([0-9]+) for ([0-9]+) intervals then (call|run|log) (.*) and wait ([0-9]+) intervals/
+      if rule =~ regex
+        rule.scan(regex) {|name, metric, operator, value, threshold, action_type, action, wait_interval|
+          @rule_name = name
+          @metric = metric
+          @operator = operator
+          @value = value.to_i
+          @cluster = cluster_name  #we know which cluster it belongs too :)
+          @threshold = threshold.to_i
+          @action_type = action_type
+          @action = action
+          @wait_interval = wait_interval.to_i
+          @check_in = @threshold
+          puts "#{@rule_name}, #{@metric}, #{@operator}, #{@value}, #{@threshold}, #{@action_type}, #{@action}, #{@wait_interval}, #{@check_in}"
+        }
+      else
+        @error.puts "ERROR: #{rule} isn't correct"
+      end
+    end
+
+    def decrement_check_in
+      @check_in = @check_in - 1
+      if @check_in <= 0
         @check_in = @threshold
-        puts "#{@rule_name}, #{@metric}, #{@operator}, #{@value}, #{@threshold}, #{@action_type}, #{@action}, #{@wait_interval}, #{@check_in}"
-      }
-    else
-      @error.puts "ERROR: #{rule} isn't correct"
-    end
-  end
-  
-  def decrement_check_in
-    @check_in = @check_in - 1
-    if @check_in <= 0
-      @check_in = @threshold
-      true
-    else
-      false
-    end
-  end
-  
-  def check_rule(data, cluster)
-    # if clusters.size == 0 then we want all clusters, otherwise, if the cluster isn't in clusters, then break out.
-    if @clusters.size > 0 && @clusters.include?(cluster) == false
-      return
-    end
-    log = STDOUT
-    pointer = data.size - 1
-    count = @threshold
-    # as long as the data meets the rule, count how many times it does
-    while (pointer >= 0 && data[pointer].include?(cluster) && check_using_operator(data[pointer][cluster][@metric].to_f, @operator, @value))
-      count = count - 1
-      pointer = pointer - 1
-      if count == 0  # if it meets the rule enough, then don't look over more data
-        break
+        true
+      else
+        false
       end
     end
-    # at this point, either it found enough, or not
-    if count == 0
-      # if it found enough: fire off method and reset threshold
-      # actionObject.send(@method_name, @rule_name)
-      begin
-        case @action_type
-        when "run"
-          puts "Run #{@action}"
-          %x[#{@action}]
-        when "call"
-          puts "Call #{@action}"
-          eval "#{@action}"
-        when "log"
-          puts "Log: #{@action}"
-          log.puts @action
+
+    def check_rule(data)
+      log = STDOUT
+      pointer = data.size - 1
+      count = @threshold
+      # as long as the data meets the rule, count how many times it does
+      while (pointer >= 0 && check_using_operator(data[pointer][@metric].to_f, @operator, @value))
+        count = count - 1
+        pointer = pointer - 1
+        if count == 0  # if it meets the rule enough, then don't look over more data
+          break
         end
-      rescue
-        puts "Error: rule #{@rule_name}'s action is invalid"
       end
-      @check_in = @wait_interval
-    else
-      # if it didn't find enough, then we want to wait as many more as are required (if it found 2 of 5, then we should check in 3 to see if all 5 met the rule)
-      @check_in = count
+      # at this point, either it found enough, or not (i do it this way so there is 1 point of exit
+      if count == 0
+        # if it found enough: fire off method and reset threshold
+        # actionObject.send(@method_name, @rule_name)
+        puts "Firing rule #{@rule_name} for cluster #{@cluster}"
+        begin
+          case @action_type
+          when "run"
+            system(@action)
+          when "call"
+            eval "#{@action}"
+          when "log"
+            log.puts @action
+          end
+        rescue
+          puts "Error: rule #{@rule_name}'s action is invalid"
+        end
+        @check_in = @wait_interval
+      else
+        # if it didn't find enough, then we want to wait as many more as are required (if it found 2 of 5, then we should check in 3 to see if all 5 met the rule)
+        @check_in = count
+      end
     end
-  end
-  
-  private
-  
-  def check_using_operator(data, operator, value)
-    case operator
-    when ">"
-      return data > value
-    when ">="
-      return data >= value
-    when "<"
-      return data < value
-    when "<="
-      return data <= value
-    when "="
-      return data == value
+
+    private
+
+    def check_using_operator(data, operator, value)
+      case operator
+      when ">"
+        return data > value
+      when ">="
+        return data >= value
+      when "<"
+        return data < value
+      when "<="
+        return data <= value
+      when "="
+        return data == value
+      end
     end
   end
 end
